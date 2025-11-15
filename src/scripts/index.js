fe1396f3f8cab935596621cc72e701359fd06592
Niel-D22 <danielwarouw01@gmail.com>
2025-11-15 13:02:35 +0800

    oush

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,18 @@
 import "../styles/styles.css";
 import App from "./pages/app.js";
 import PushNotificationInit from './init-push.js';
+import L from "leaflet";
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const app = new App({
   content: document.getElementById("main-content"),
